5d1215b72eec82169a25ab817e337720e69f4efa
jofre.aleman.7e7 <jofre.aleman.7e7@itb.cat>
2023-10-09 12:37:35 +0200

    JAS1

--- Pr2_TipusDades_23_24/E1.py
+++ Pr2_TipusDades_23_24/E1.py
@@ -4,12 +4,12 @@ Jofre Aleman i Daniel Arco
 ASIXc M03-UF1 A2 pr2
 Exercici 1: Demanar el diàmetre d'una pizza rodona i imprimeix la seva superfície.
 """
-
+#Importem el dicionari math
 import math
 
-r = float(input("Vols saber la superfície d'una pizza? Doncs passem el seu radi en cm!\n : "))
+diametre = float(input("Vols saber la superfície d'una pizza? \nDoncs passem el seu diàmetre en cm!: "))
+r = diametre/2
 
+superficie = math.pi*r**2
 
-supericie =
-
-ddfdfdfdf
+print(f'La superfície de la teva pizza és de: {superficie:.2f}cm²')
