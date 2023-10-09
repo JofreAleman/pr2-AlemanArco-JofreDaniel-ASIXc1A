f8c7f62149fb3b46493cc672e79963bb88a9a9e8
daniel.arco.7e5 <daniel.arco.7e5@itb.cat>
2023-10-09 12:09:24 +0200

    commit

--- Pr2_TipusDades_23_24/E1.py
+++ Pr2_TipusDades_23_24/E1.py
@@ -1,1 +1,8 @@
-oip0+oioin+o
+##Demanar el diàmetre d'una pizza rodona i imprimeix la seva superfície. Pots usar Math.PI per escriure el valor de Pi.
+import math
+
+superficie_input = float(input("Vols saber la superfície d'una pizza? Doncs passem la seva superficie!\n : "))
+
+
+supericie_output =
+
--- Pr2_TipusDades_23_24/E5.py
+++
@@ -1,3 +0,0 @@
-##Demanar el diàmetre d'una pizza rodona i imprimeix la seva superfície. Pots usar Math.PI per escriure el valor de Pi.
-
-superficie = float(int("Vols saber el "))
