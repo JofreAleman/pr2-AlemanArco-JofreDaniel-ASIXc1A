ef1f03e5c8810b801d4a42401c403ede10066508
jofre.aleman.7e7 <jofre.aleman.7e7@itb.cat>
2023-10-09 13:04:04 +0200

    JAS3

---
+++ Pr2_TipusDades_23_24/E3.py
@@ -0,0 +1,11 @@
+"""
+Jofre Aleman i Daniel Arco
+09/10/2023
+ASXIc M03-UF1 A2 pr2
+Exercici 3:Escriu un programa que llegeixi 5 enters. El primer i el segon creen un rang, el tercer i el quart creen un
+altre rang. Mostra True si el cinquè valor, està comprès dins els dos rangs, si no False
+"""
+
+numeros = input("Escriu una llista de 5 nombres enters aleatoris, seprats entre ells per ',': ")
+
+numeros.split
